0f0318df00a5317f1224cc9a2a6c104da6fdd918
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2022-07-08 23:33:40 +0300

    step1-2 feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+// import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  handleClick = (key) => {
+    this.setState(prev => {
+      return {
+        [key]:prev[key]+1
+      }
+    })
+  }
+
+  countTotalFeedback = () => {
+    return Object.values(this.state)
+      .reduce((total, item) => total + item, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round(this.state.good * 100 / this.countTotalFeedback())
+  };
+
+  render() {
+    
+    const total = this.countTotalFeedback();
+    const goodPercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+        {/* <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleIncrement}
+          ></FeedbackOptions> */}
+        <div>
+          {Object.keys(this.state)
+            .map(key =>
+              <button
+                key={key}
+                onClick={() => this.handleClick(key)}>
+                {key}
+              </button>)}
+        </div>
+        <h2>Statistics</h2>
+        <ul>{Object.keys(this.state)
+          .map(key =>
+            <li key={key}>
+              {key}: {this.state[key]}
+            </li>)}
+        <li>Total:{total}</li>
+          <li>Positive feedback: {goodPercentage}</li>
+          </ul>
+        </>)
+ }
+
 };
